8113361e7ef995c8d7792956f473a4e8e912f8bf
Julián Manrique <jmanrique@truora.com>
2019-11-12 23:25:08 -0500

    Add department discrimination to converters

---
+++ ETL/account-hierarchy/convert-gastos-depto.js
@@ -0,0 +1,100 @@
+const Papa = require('papaparse');
+const fs = require('fs');
+
+const convert = (path, saveAccounts, saveTrees) => {
+  const file = fs.readFileSync(path).toString();
+  const result = Papa.parse(file, {header: true});
+  print("[Rows read]: ", result.data.length)
+
+  const accounts = {};
+  const deptos = getAllDepartments(result.data);
+
+  for(const depto of deptos) {
+    for (const tx of result.data) {
+      // Only sum tx's of the same department
+      if(depto !== tx.NOM_DEPAR) {
+        break;
+      }
+
+      const hierarchy = tx.COD_CUENTA.trim().split('.');
+      let parent = accounts;
+  
+      for (const level of hierarchy) {
+          createIfDoesNotExist(parent, level);
+          parent = parent[level];
+      }
+  
+      if (parent.name && parent.name !== tx.NOM_CUENTA) {
+        print('[diferentes]: ', parent.name, tx.NOM_CUENTA);
+      }
+  
+      // parent.value += parseInt(tx.APR_DEFINITIVA.replace('.', ''), 10);
+      parent.value += parseInt(tx.NETO_CDP.replace('.', ''), 10);
+      parent.name = tx.NOM_CUENTA;
+      parent.count += 1;
+    }
+  
+    if (saveAccounts) {
+      fs.writeFileSync(`gastos-depto/spend-accounts.json`, JSON.stringify(accounts));
+    }
+  
+    const tree = convertToTree(accounts, []);
+    if (saveTrees) {
+      fs.writeFileSync(`gastos-depto/${depto}-tree.json`, JSON.stringify(tree));
+    }
+  }
+
+  
+}
+
+const getAllDepartments = (data) => {
+  const departments = {};
+  for(const tx of data) {
+    if(!departments[tx.NOM_DEPAR]) {
+      departments[tx.NOM_DEPAR] = true;
+    }
+  }
+  return Object.keys(departments);
+}
+
+const createIfDoesNotExist = (parent, level) => {
+  if(parent[level] === undefined) {
+    parent[level] = { 
+      count: 0,
+      value: 0,
+    };
+  }
+}
+
+const convertToTree = (accounts) => {
+  const children = Object.keys(accounts).filter(key => key != 'value' && key != 'name' && key != 'count');
+  
+  if (children.length === 0){
+    return accounts.value
+  }
+
+  const nodes = [];
+
+  for (const childKey of children) {
+    const childAccounts = accounts[childKey];
+    const childNode = {
+      // Use key and values to match d3 data structure requirement
+      key: childAccounts.name,
+      values: convertToTree(childAccounts),
+    };
+
+    // If the children is a number, rename the attribute to 'value'
+    if (typeof childNode.values === 'number') {
+      childNode.value = childNode.values;
+      delete childNode.values;
+    }
+
+    nodes.push(childNode);
+  }
+
+  return nodes;
+}
+
+const print = (...args) => console.log(...args);
+
+convert('./../data/Gastos filtrado.csv', false, true);
---
+++ ETL/account-hierarchy/convert-ingresos-depto.js
@@ -0,0 +1,100 @@
+const Papa = require('papaparse');
+const fs = require('fs');
+
+const convert = (path, saveAccounts, saveTrees) => {
+  const file = fs.readFileSync(path).toString();
+  const result = Papa.parse(file, {header: true});
+  print("[Rows read]: ", result.data.length)
+
+  const accounts = {};
+  const deptos = getAllDepartments(result.data);
+
+  for(const depto of deptos) {
+    for (const tx of result.data) {
+      // Ignore tx's of different departments
+      if(depto !== tx.NOM_DEPAR) {
+        break;
+      }
+
+      const hierarchy = tx.COD_CUENTA.trim().split('.');
+      let parent = accounts;
+  
+      for (const level of hierarchy) {
+          createIfDoesNotExist(parent, level);
+          parent = parent[level];
+      }
+  
+      if (parent.name && parent.name !== tx.NOM_CUENTA) {
+        print('[diferentes]: ', parent.name, tx.NOM_CUENTA);
+      }
+  
+      // parent.value += parseInt(tx.PRE_DEFINITIVO.replace('.', ''), 10);
+      parent.value += parseInt(tx.NETO_RECAUDO_EN_EFECTIVO.replace('.', ''), 10);
+      parent.name = tx.NOM_CUENTA;
+      parent.count += 1;
+    }
+  
+    if (saveAccounts) {
+      fs.writeFileSync(`ingresos-depto/${depto}-accounts.json`, JSON.stringify(accounts));
+    }
+  
+    const tree = convertToTree(accounts, []);
+    if (saveTrees) {
+      fs.writeFileSync(`ingresos-depto/${depto}-tree.json`, JSON.stringify(tree));
+    }
+  }
+
+  
+}
+
+const getAllDepartments = (data) => {
+  const departments = {};
+  for(const tx of data) {
+    if(!departments[tx.NOM_DEPAR]) {
+      departments[tx.NOM_DEPAR] = true;
+    }
+  }
+  return Object.keys(departments);
+}
+
+const createIfDoesNotExist = (parent, level) => {
+  if(parent[level] === undefined) {
+    parent[level] = { 
+      count: 0,
+      value: 0,
+    };
+  }
+}
+
+const convertToTree = (accounts) => {
+  const children = Object.keys(accounts).filter(key => key != 'value' && key != 'name' && key != 'count');
+  
+  if (children.length === 0){
+    return accounts.value
+  }
+
+  const nodes = [];
+
+  for (const childKey of children) {
+    const childAccounts = accounts[childKey];
+    const childNode = {
+      // Use key and values to match d3 data structure requirement
+      key: childAccounts.name,
+      values: convertToTree(childAccounts),
+    };
+
+    // If the children is a number, rename the attribute to 'value'
+    if (typeof childNode.values === 'number') {
+      childNode.value = childNode.values;
+      delete childNode.values;
+    }
+
+    nodes.push(childNode);
+  }
+
+  return nodes;
+}
+
+const print = (...args) => console.log(...args);
+
+convert('./../data/Ingresos filtrado.csv', false, true);
